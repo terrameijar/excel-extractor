83d0de69fc7f0e0b5a8229791dd8142d1d330319
Vuyisile Ndlovu <terrameijar@gmail.com>
2017-09-04 10:19:37 +0200

    Moved common declarations to a config module

---
+++ config.py
@@ -0,0 +1,8 @@
+import datetime
+
+
+
+today = datetime.datetime.today()
+thirty_days_ago = today - datetime.timedelta(days=30)
+fifteen_days_ago = today - datetime.timedelta(days=15)
+seven_days_ago = today - datetime.timedelta(days=7)
