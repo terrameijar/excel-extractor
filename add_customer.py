992368eb73a3880137ed62d0e628a18928ecfbd8
Vuyisile Ndlovu <terrameijar@gmail.com>
2017-09-04 10:18:41 +0200

    Added support for searching for records added within 30 days

--- add_customer.py
+++ add_customer.py
@@ -1,20 +1,51 @@
 # Adds new customers to the customer database
 from faker import Faker
+import openpyxl
 
 
 def add_customer(n):
-    # Adds new n customers
-    f = Faker()
-    num_cust = n+1
-    # Generate customer details for n customers.
-    for customer in range(1, num_cust):
-        print customer , ":", f.first_name(), f.last_name()
-        print " " , f.phone_number()
-        print " ", f.email()
-        print " ", f.date_time_this_month()
-        print "---------------------"
+    """Creates dummy customers.
 
-    # TODO: Add code to write these details to a database
+    Returns:
+        list. List of dictionaries containing customer info.
+    """
+
+    f = Faker()
+    num_cust = n
+    customers = []
+    for customer in range(1, num_cust + 1):
+        cust_data = {'first_name': f.first_name(), 'last_name': f.last_name(),
+                     'phone': f.phone_number(), 'email': f.email(),
+                     'date': f.date_time_this_year()}
+
+        customers.append(cust_data)
+    return customers
+
+
+def update_worksheet(customer_list):
+    """Adds customer(s) to the database.
+
+    Attributes:
+        customer_list: list. A list of dictionaries containing customer info.
+    """
+
+    # Write customer details to a database
+    wb = openpyxl.load_workbook('customer_data-edited-dates.xlsx')
+    sheet = wb.active
+
+    for row in range(len(customer_list)):
+        # add 2 to row to account for header row and zero based indexing.
+        sheet.cell(row=row + 2, column=1).value = \
+            customer_list[row]['first_name']
+        sheet.cell(row=row + 2, column=2).value = \
+            customer_list[row]['last_name']
+        sheet.cell(row=row + 2, column=3).value = \
+            customer_list[row]['phone']
+        sheet.cell(row=row + 2, column=4).value = \
+            customer_list[row]['email']
+        sheet.cell(row=row + 2, column=5).value = \
+            customer_list[row]['date']
+    wb.save('customer_data-edited-dates.xlsx')
 
 if __name__ == "__main__":
-    add_customer(5)
+    update_worksheet(add_customer(100))
--- retrieve_data.py
+++ retrieve_data.py
@@ -1,1 +1,67 @@
 # Retrieves data from the customer database
+# pick customers added 30 days ago.
+# retrieve customers added 15 days ago
+# retrieve customers added 7 days ago.
+# write retrieved data to a separate spreadsheet
+# sync new spreadsheet to google sheets
+# update sheet names on each spreadsheet written to.
+import openpyxl
+import config
+import pdb
+
+def main():
+    thirty_days()
+
+def get_spreadsheet_data():
+    wb = openpyxl.load_workbook('customer_data-edited-dates.xlsx')
+    sheet = wb.active
+    # To get rows from worksheet
+    rows = [row for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row)]
+    return rows
+
+def string_to_index(col):
+    '''Returns Cell column index from string'''
+    return openpyxl.utils.column_index_from_string(col)
+
+def seven_days():
+    pass
+
+
+def fifteen_days():
+    pass
+
+
+def thirty_days():
+    thirty_day_ws = openpyxl.Workbook()
+    sheet = thirty_day_ws.active
+
+    thirty_days_ago = config.thirty_days_ago
+    rows = get_spreadsheet_data()
+    # rows is a list of tuples
+    # each row looks like (<Cell Sheet.A2>, <Cell Sheet.B2>, <Cell Sheet.C2>)
+    retrieved_data = []
+
+    # Search for records added in the last 30 days
+    for row in rows:
+        if row[4].value >= thirty_days_ago:
+            retrieved_data.append(row)
+    try:
+        for rownum in range(len(retrieved_data) + 1):
+
+            for row_ in retrieved_data:
+                for item in row_:
+                    sheet.cell(row=rownum + 1,column=string_to_index(item.column)).value \
+                        = retrieved_data[rownum][string_to_index(item.column) -1].value
+    except IndexError:
+        pass
+
+    thirty_day_ws.save('cust-30-days.xlsx')
+
+
+    # picks customers entered within 30 days
+    # data in workbook is now in rows and columns. Need to make it dict
+    # https://stackoverflow.com/questions/6900955/python-convert-list-to-dictionary
+
+
+if __name__ == '__main__':
+    main()
